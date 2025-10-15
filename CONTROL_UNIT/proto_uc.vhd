8c92db26a9468d7be9c09f67bbe22aeb6aada222
Eduardo Dias <142503604+ediasv@users.noreply.github.com>
2025-10-15 19:13:16 -0300

    WIP

--- CONTROL_UNIT/proto_uc.vhd
+++ CONTROL_UNIT/proto_uc.vhd
@@ -1,14 +1,20 @@
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 entity proto_uc is
-  port(uc_data_in : in unsigned(6 downto 0);
-       uc_data_out : out unsigned(6 downto 0)
-  );
+  port (uc_data_in  : in  unsigned(14 downto 0);
+        uc_data_out : out unsigned(6 downto 0)
+       );
 end entity;
 
 architecture a_proto_uc of proto_uc is
+  signal opcode : unsigned(3 downto 0);
+  signal addr   : unsigned(6 downto 0);
 begin
-  uc_data_out <= uc_data_in + 1;
-end architecture;
+  opcode <= uc_data_in(14 downto 11);
+  addr   <= uc_data_in(6 downto 0);
+
+  uc_data_out <= addr when opcode = "1111" else -- jump
+                 addr + 1;  -- NOP
+ end architecture;
